0b996005a1b90d7141786e914b56dd7c379a2a7f
伍朝辉 <337883612@qq.com>
2016-09-30 18:53:38 +0800

    新增版本升级检测接口

---
+++ Td.Kylin.Cache.WebApi/AppVersion/AppVersion .cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace Td.Kylin.Cache.WebApi
+{
+    public static class AppVersionCache
+    {
+        private static Dictionary<string, List<AppVersion>> _allVersion;
+
+        private readonly static object _locker = new object();
+
+        private static DateTime LastUpdateTime = DateTime.Now.AddYears(-1);
+
+        public static Dictionary<string, List<AppVersion>> AllVersion
+        {
+            get
+            {
+                if (_allVersion == null || LastUpdateTime.AddMinutes(30) < DateTime.Now)
+                {
+                    lock (_locker)
+                    {
+                        if (_allVersion == null || LastUpdateTime.AddMinutes(30) < DateTime.Now)
+                        {
+                            _allVersion = GetVersion();
+
+                            LastUpdateTime = DateTime.Now;
+                        }
+                    }
+                }
+
+                return _allVersion;
+            }
+        }
+
+        private static Dictionary<string, List<AppVersion>> GetVersion()
+        {
+            Dictionary<string, List<AppVersion>> dic = new Dictionary<string, List<AppVersion>>();
+
+            try
+            {
+                string xmlPath = Path.Combine(AppContext.BaseDirectory, "appversion.xml");
+
+                XElement xe = XElement.Load(xmlPath);
+
+                //获取客户端
+                IEnumerable<XElement> apps = from em in xe.Elements("app")
+                                             select em;
+
+                foreach (var app in apps)
+                {
+                    string appCode = app.Attribute("code").Value;
+
+                    List<AppVersion> versionList = new List<AppVersion>();
+
+                    foreach (var vs in app.Elements("version"))
+                    {
+                        var version = vs.Attribute("code").Value;
+
+                        bool isForcedUpgrade = Convert.ToBoolean(vs.Attribute("isForcedUpgrade").Value);
+
+                        string url = vs.Attribute("url").Value;
+
+                        versionList.Add(new AppVersion
+                        {
+                            Version = version,
+                            IsForcedUpgrade = isForcedUpgrade,
+                            Url = url
+                        });
+                    }
+
+                    dic.Add(appCode.ToLower(), versionList);
+                }
+            }
+            catch
+            {
+                dic = new Dictionary<string, List<AppVersion>>();
+            }
+
+            return dic;
+        }
+    }
+
+    public class AppVersion
+    {
+        public string Version { get; set; }
+
+        public bool IsForcedUpgrade { get; set; }
+
+        public string Url { get; set; }
+    }
+}
---
+++ Td.Kylin.Cache.WebApi/Controllers/CheckVersionController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Td.Kylin.WebApi;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Td.Kylin.Cache.WebApi.Controllers
+{
+    [Route("api/checkversion")]
+    public class CheckVersionController : BaseController
+    {
+        [HttpGet]
+        public IActionResult CheckVersion(string appcode)
+        {
+            if (string.IsNullOrWhiteSpace(appcode)) appcode = string.Empty;
+
+            appcode = appcode.ToLower();
+
+            List<AppVersion> data = new List<AppVersion>();
+
+            try
+            {
+                if (AppVersionCache.AllVersion.ContainsKey(appcode))
+                {
+                    data = AppVersionCache.AllVersion[appcode];
+                }
+            }
+            catch
+            {
+                data = null;
+            }
+
+            return Success(data);
+        }
+    }
+}
